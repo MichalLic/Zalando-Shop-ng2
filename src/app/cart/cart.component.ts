3efdf77a662a34c17c3ca96265da1e2fbd3159ed
Michal <m14lickiewicz@gmail.com>
2017-09-06 12:20:54 +0200

    Update cart module.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ProductsService} from '../products.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,10 +8,14 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-
-  constructor() {
-  }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
+    this.productsService.getOrderedProducts()
+      .subscribe(
+        data => console.log(data)
+      );
+
   }
+
 }
