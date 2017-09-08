45dfe7bc4fa9cabd3c0bfb75b97d940cb7fe0e78
Michal <m14lickiewicz@gmail.com>
2017-09-08 12:16:48 +0200

    Add logout method.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../auth/auth.service';
 
 @Component({
@@ -8,9 +8,14 @@ import {AuthService} from '../../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
